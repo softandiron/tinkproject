2f8d30ef87f23783a38d045b5b251a8bbd1efeae
Alexey Stafievsky <Stafievsky@avp.ru>
2022-01-01 23:13:59 +0300

    Add a separate function for creating a report & fix unit tests

--- test/test_main.py
+++ test/test_main.py
@@ -12,29 +12,29 @@ class TestIISDeductCalculation(unittest.TestCase):
     @classmethod
     def setUpClass(self):
         # создать несколько объектов операций
-        main.my_operations = [
+        self.my_operations = [
             # покупка за 2021 год - не должно посчитать
             PortfolioOperation(op_type='Buy',
                                op_date=datetime(2021, 7, 16, 10, 40, 47, 818000, tzinfo=timezone(timedelta(seconds=10800))),
                                op_currency='RUB', op_payment=Decimal('0'), op_ticker='SBMX',
                                op_payment_rub=Decimal('-1794'), op_figi='BBG00M0C8YM7',
-                               op_in_last_12_months=True),
+                               op_status=''),
             # пополнение за 2021 год
             PortfolioOperation(op_type='PayIn', op_date=datetime(2021, 1, 12, 10, 48, 29, tzinfo=timezone(timedelta(seconds=10800))),
                                op_currency='RUB', op_payment=Decimal('10000.0'), op_ticker='None',
-                               op_payment_rub=Decimal('10000.0'), op_figi=None, op_in_last_12_months=True),
+                               op_payment_rub=Decimal('10000.0'), op_figi=None, op_status=''),
             # пополнение за 2021 год
             PortfolioOperation(op_type='PayIn', op_date=datetime(2021, 1, 12, 10, 48, 29, tzinfo=timezone(timedelta(seconds=10800))),
                                op_currency='RUB', op_payment=Decimal('10000.0'), op_ticker='None',
-                               op_payment_rub=Decimal('10000.0'), op_figi=None, op_in_last_12_months=True),
+                               op_payment_rub=Decimal('10000.0'), op_figi=None, op_status=''),
             # пополнение за 2019 год с превышением максимальной суммы пополнения
             PortfolioOperation(op_type='PayIn', op_date=datetime(2019, 1, 12, 10, 48, 29, tzinfo=timezone(timedelta(seconds=10800))),
                                op_currency='RUB', op_payment=Decimal('2000000.0'), op_ticker='None',
-                               op_payment_rub=Decimal('2000000.0'), op_figi=None, op_in_last_12_months=True),
+                               op_payment_rub=Decimal('2000000.0'), op_figi=None, op_status=''),
             # пополнение за 2020 год в долларах
             PortfolioOperation(op_type='PayIn', op_date=datetime(2020, 1, 12, 10, 48, 29, tzinfo=timezone(timedelta(seconds=10800))),
                                op_currency='USD', op_payment=Decimal('2000000.0'), op_ticker='None',
-                               op_payment_rub=Decimal('2000000.0'), op_figi=None, op_in_last_12_months=True),
+                               op_payment_rub=Decimal('2000000.0'), op_figi=None, op_status=''),
         ]
         # итого должно быть 20000*0,13 = 2600 за 2021 год
         # 2020 год - ничего - так как пополнение было в долларах
@@ -44,20 +44,20 @@ class TestIISDeductCalculation(unittest.TestCase):
         logger = logging.getLogger()
         logger.setLevel(logging.CRITICAL)
         main.logger = logging.getLogger("calculator")
-        main.sum_profile = {}
-        main.sum_profile['broker_account_type'] = "Tinkoff"
+        sum_profile = {}
+        sum_profile['broker_account_type'] = "Tinkoff"
         main.logger.setLevel(logging.DEBUG)
         with self.assertLogs(logger=main.logger, level=logging.DEBUG) as logs:
-            self.assertIsNone(main.calculate_iis_deduction())
+            self.assertIsNone(main.calculate_iis_deduction(self.my_operations, sum_profile))
         self.assertEqual(logs.records[0].getMessage(), "account is not of IIS Type")
 
     def test_deduct_IIS_account(self):
         logger = logging.getLogger()
         logger.setLevel(logging.CRITICAL)
         main.logger = logging.getLogger("calculator")
-        main.sum_profile = {}
-        main.sum_profile['broker_account_type'] = "TinkoffIis"
-        self.assertIsNotNone(main.calculate_iis_deduction())
+        sum_profile = {}
+        sum_profile['broker_account_type'] = "TinkoffIis"
+        self.assertIsNotNone(main.calculate_iis_deduction(self.my_operations, sum_profile))
 
         test_etalon = {2021: {'pay_in': Decimal('20000.00'), 'base': Decimal('20000.0'),
                               'deduct': Decimal('2600.00')},
@@ -66,7 +66,7 @@ class TestIISDeductCalculation(unittest.TestCase):
                        0: Decimal('54600.00')}
 
         with self.assertLogs(logger=main.logger) as logs:
-            self.assertDictEqual(test_etalon, main.calculate_iis_deduction())
+            self.assertDictEqual(test_etalon, main.calculate_iis_deduction(self.my_operations, sum_profile))
         messages = []
         for record in logs.records:
             messages.append(record.getMessage())
